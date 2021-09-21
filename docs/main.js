f649f9e0d608095a072eee95dfd4aca8c69b1c83
MastarCheeze <91102404+MastarCheeze@users.noreply.github.com>
2021-09-21 18:34:24 +0800

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,8 +1,10 @@
 console.log("yeah");
 $("#button").on("mousedown", function() {
+  console.log("you pressed");
   $("#text").text("lovely image of dead crocodile");
   $("#image").attr("src", "img/dead.jpg");
 }).on("mouseup mouseleave", function() {
+  console.log("you no pressed anymore");
   $("#text").text("lovely image of crocodile");
   $("#image").attr("src", "img/alive.jpeg");
 });
