d18121ca8e5feb29458a9e84076fe450939af1d8
MastarCheeze <91102404+MastarCheeze@users.noreply.github.com>
2021-09-21 18:38:08 +0800

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,10 +1,9 @@
-console.log("yeah");
-$("#button").on("mousedown", function() {
-  console.log("you pressed");
-  $("#text").text("lovely image of dead crocodile");
-  $("#image").attr("src", "img/dead.jpg");
-}).on("mouseup mouseleave", function() {
-  console.log("you no pressed anymore");
-  $("#text").text("lovely image of crocodile");
-  $("#image").attr("src", "img/alive.jpeg");
+$(document).ready(function() {
+  $("#button").on("mousedown", function() {
+    $("#text").text("lovely image of dead crocodile");
+    $("#image").attr("src", "img/dead.jpg");
+  }).on("mouseup mouseleave", function() {
+    $("#text").text("lovely image of crocodile");
+    $("#image").attr("src", "img/alive.jpeg");
+  });
 });
