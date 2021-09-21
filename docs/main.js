a0a20becbf0a597924622a94725fbabf3fa72b70
MastarCheeze <91102404+MastarCheeze@users.noreply.github.com>
2021-09-21 18:29:10 +0800

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -2,6 +2,6 @@ $("#button").on("mousedown", function() {
   $("#text").text("lovely image of dead crocodile");
   $("#image").attr("src", "img/dead.jpg");
 }).on("mouseup mouseleave", function() {
-  $("#text").text("lovely image of crocodile")
+  $("#text").text("lovely image of crocodile");
   $("#image").attr("src", "img/alive.jpeg");
 });
